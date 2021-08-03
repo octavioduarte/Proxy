fb44cc3a8f8034ac688574fdbc412a3978d3d0a8
octavio duarte <octavioduarte2105@gmail.com>
2021-08-02 22:39:46 -0300

    feat: create main route to process all requests

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import express, { Express, Request, Response } from 'express'
-import { loadMiddleware, LoadAPI } from '@/middlewares'
+import { loadMiddleware, LoadAPIS, LoadRoute } from '@/middlewares'
 import { env } from '@/config/env'
+import { APIType } from './types'
 
 const app = express()
 
 env.HTTP_METHODS.forEach((method: keyof Express) => {
     app[method](
         "*",
-        loadMiddleware(new LoadAPI()),
-        async (req: Request, res: Response) => res.json({ url: req.apis })
+        loadMiddleware(new LoadAPIS()),
+        loadMiddleware(new LoadRoute()),
+        async (req: Request, res: Response) => {
+            console.log(req.body)
+            res.json({ "api-found": req.api })
+        }
     )
 })
 
@@ -18,13 +23,8 @@ app.listen(env.HTTP_PORT, () => { console.log(`Proxy running at port: ${env.HTTP
 declare global {
     namespace Express {
         export interface Request {
-            apis?: {
-                authenticated: boolean
-                enabled: boolean
-                key: string
-                prefix: string
-                url: string
-            }[]
+            allServers?: APIType[],
+            api?: APIType
         }
     }
 }
