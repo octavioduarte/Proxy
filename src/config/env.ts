5c18a342966e443a4abbfd28c4eeb1d3426bfde1
octavio duarte <octavioduarte2105@gmail.com>
2021-08-07 01:44:30 -0300

    feat: add Mongo URI on file with settings of project

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,7 @@
 export const env = {
     HTTP_METHODS: ["get", "post", "delete", "patch", "put"],
-    HTTP_PORT: 8000 || process.env.HTTP_PORT
+    HTTP_PORT: 8000 || process.env.HTTP_PORT,
+    DB: {
+        MONGO_URI: process.env.MONGO_URI
+    }
 }
