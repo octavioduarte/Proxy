09a1c32e61e417f3f9dbd1af578f3f236bc00a51
octavio duarte <octavioduarte2105@gmail.com>
2021-08-07 02:17:01 -0300

    refactor: load dotenv package before read .env vars

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv-flow';
+
+dotenv.config()
+
 export const env = {
     HTTP_METHODS: ["get", "post", "delete", "patch", "put"],
     HTTP_PORT: 8000 || process.env.HTTP_PORT,
