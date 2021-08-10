141b48971fd9f1b4a397078a8f128e6ca07922de
octavio duarte <octavioduarte2105@gmail.com>
2021-08-10 15:01:47 -0300

    feat: add redis settings on env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,6 +6,8 @@ export const env = {
     HTTP_METHODS: ["get", "post", "delete", "patch", "put"],
     HTTP_PORT: 8000 || process.env.HTTP_PORT,
     DB: {
-        MONGO_URI: process.env.MONGO_URI
+        MONGO_URI: process.env.MONGO_URI,
+        REDIS_PORT: process.env.REDIS_PORT,
+        REDIS_HOST: process.env.REDIS_HOST
     }
 }
