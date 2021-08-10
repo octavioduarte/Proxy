3d00b6959f16ac698d369598153e74956880bc40
octavio duarte <octavioduarte2105@gmail.com>
2021-08-10 18:56:13 -0300

    refactor: implement structure to get values of servers rules from redis

--- src/middlewares/load-apis.ts
+++ src/middlewares/load-apis.ts
@@ -1,18 +1,28 @@
 import { Request } from 'express'
-import { internalError } from '@/helpers'
-import { APIType, ClassMiddlewares, HttpResponse } from '@/types'
-import { MongoHelper } from '@/db'
 import { env } from '@/config/env'
+import { internalError } from '@/helpers'
+import { MongoHelper, RedisHelper } from '@/db'
+import { APIType, ClassMiddlewares, HttpResponse } from '@/types'
 
 export class LoadAPIS implements ClassMiddlewares {
     async handle(req: Request): Promise<HttpResponse> {
+
         try {
-            const mongoHelper = new MongoHelper()
-            await mongoHelper.connect(env.DB.MONGO_URI)
-            const serversRulesCollection = await mongoHelper.getCollection('servers-rules')
-                        
-            req.allServers = await serversRulesCollection.find().toArray() as unknown as APIType[]
-            await mongoHelper.disconnect()
+            const redisHelper = new RedisHelper()
+            const serversRules: APIType[] = await redisHelper.get(env.DB.SERVERS_RULES_KEY) as APIType[]
+
+            if (!serversRules) {
+                const mongoHelper = new MongoHelper()
+                await mongoHelper.connect(env.DB.MONGO_URI)
+                const serversRulesCollection = await mongoHelper.getCollection('servers-rules')
+
+                const result = await serversRulesCollection.find().toArray()
+                redisHelper.set(env.DB.SERVERS_RULES_KEY, JSON.stringify(result))
+                req.allServers = result as unknown as APIType[]
+                await mongoHelper.disconnect()
+            } else {
+                req.allServers = serversRules
+            }
         } catch (error) {
             return internalError(error)
         }
